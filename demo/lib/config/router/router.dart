8f2e33e4e72a0306e77f998c2aac0eb3a84a6b52
Andressalasf <humkyjimp@gmail.com>
2025-08-30 07:45:53 -0500

    Añadiendo el menu de aplicaciones tipo burger, ademas de configurar la rutas a travez de un constructor

--- demo/lib/config/router/router.dart
+++ demo/lib/config/router/router.dart
@@ -1,11 +1,28 @@
+import 'package:demo/config/router/app_router.dart';
 import 'package:demo/presentation/screens/screen.dart';
+import 'package:flutter/material.dart';
 
 class Routers{
-  static route(){
-      return {
-        '/': (context) => const HomeScreen(),
-        'button': (context) => const ButtonScreen(),
-        'card': (context) => const CardScreen()
-      };
-  }
+
+static final List<AppRouter> pages = [
+  AppRouter(patch: '/',
+   title: 'Home',
+    description: "Pantalla inicial",
+    icon: Icons.home,
+    context: (context) => const HomeScreen()),
+    AppRouter(patch: 'button',
+   title: 'Button',
+    description: "Boton de carga",
+    icon: Icons.radio_button_on_sharp,
+    context:(context) => const ButtonScreen()),
+    AppRouter(patch: 'card',
+   title: 'Card',
+    description: "Card",
+    icon: Icons.card_membership_sharp,
+    context:  (context) => const CardScreen()),
+];
+
+
+  static route() =>{ for (var page in pages) page.patch : page.context};
+      
 }
--- demo/lib/presentation/screens/home/home.dart
+++ demo/lib/presentation/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:demo/config/router/router.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,9 +7,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: FloatingActionButton(onPressed: (){
-       Navigator.pushNamed(context, 'card'); 
-      }),),
+      appBar: AppBar(title: const Text("Home screen"),
+      backgroundColor: Colors.red),
+      body: Center(
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, 'home');
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ...Routers.pages.map((res) {
+              return ListTile(
+                title:Text(res.title),
+                subtitle: Text(res.description),
+                onTap: (){
+                  Navigator.pushNamed(context, res.patch);
+                },
+                leading: Icon(res.icon),
+              );
+            }).toList()
+          ],
+        ),
+      ),
     );
   }
 }
